43f4ce4a53354c4a27c387ef02b97d787122a13b
Baachus <bc.robert@gmail.com>
2024-03-10 11:49:28 -0500

    removed js

--- scooby_doo_test/pageObjects/pages/family_tree.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FamilyTreePage = void 0;
-class FamilyTreePage {
-    constructor(page) {
-        this.page = page;
-    }
-}
-exports.FamilyTreePage = FamilyTreePage;
